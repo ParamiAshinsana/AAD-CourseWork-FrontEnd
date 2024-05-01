af1c62f7ee6907a0f0e79917ac257d30a18c6c78
Parami <paramiashinsana@gmail.com>
2024-05-01 21:16:00 +0530

    update customer_services.js

--- js/customer_service.js
+++ js/customer_service.js
@@ -1,15 +1,24 @@
 getAllCustomer();
 
 function getAllCustomer() {
-    $("#customer-tbl-body").empty();
+
 
     $.ajax({
         method:"GET",
         url:"http://localhost:8080/api/v1/customers/getAllCustomer",
         async:true,
         success: function(data) {
-            data.forEach(function(customer) {
-                let record = `<tr><td class="customerId">${customer.customerId}</td><td class="customerName">${customer.customerName}</td><td class="customerAddress">${customer.customerAddress}</td><td class="customerNumber">${customer.customerNumber}</td></tr>`;
+            data.forEach(function(customerService) {
+                let record = `<tr><td class="customerCode">${customerService.customerCode}</td>
+                                         <td class="customerName">${customerService.customerName}</td>
+                                         <td class="customerDOB">${customerService.customerDOB}</td>
+                                         <td class="customerGender">${customerService.customerGender}</td>
+                                         <td class="customerJoinDate">${customerService.customerJoinDate}</td>
+                                         <td class="customerLoyaltyPoints">${customerService.customerLoyaltyPoints}</td>
+                                         <td class="loyaltyLevel">${customerService.loyaltyLevel}</td>
+                                         <td class="customerAddress">${customerService.customerAddress}</td>
+                                         <td class="customerContact">${customerService.customerContact}</td>
+                                         <td class="customerEmail">${customerService.customerEmail}</td></tr>`;
                 $("#customer-tbl-body").append(record);
             });
         }
