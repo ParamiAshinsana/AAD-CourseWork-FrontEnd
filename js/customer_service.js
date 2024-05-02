6040ab1e3e50d396cda30b0f62bcd0e56b66325d
Parami <paramiashinsana@gmail.com>
2024-05-02 15:44:25 +0530

    update customer_services.js

--- js/customer_service.js
+++ js/customer_service.js
@@ -1,5 +1,5 @@
 getAllCustomer();
-
+var row_index = null;
 
 
 // Customers Save
@@ -44,6 +44,26 @@ function saveCustomer(){
     })
 }
 
+// Customer Delete
+
+function deleteCustomer(){
+    let custCode = $('#customer_code').val();
+
+    $.ajax({
+        method:"DELETE",
+        url:"http://localhost:8080/api/v1/customers/deleteCustomer/"+custCode,
+        async:true,
+
+        success: function (data){
+            alert("Deleted!!!")
+            getAllCustomer();
+        },
+        error: function (xhr, exception){
+            alert("Error!!!")
+        },
+
+    })
+}
 
 
 // Get All Customers
@@ -70,3 +90,30 @@ function getAllCustomer() {
         }
     });
 }
+
+// when click a row
+$("#customer-tbl-body").on("click", "tr", function() {
+    row_index = $(this).index();
+
+    let custCode = $(this).find(".customerCode").text();
+    let custName = $(this).find(".customerName").text();
+    let custDOB = $(this).find(".customerDOB").text();
+    let custGender = $(this).find(".customerGender").text();
+    let custJoinDate = $(this).find(".customerJoinDate").text();
+    let custLoyaltyPoints = $(this).find(".customerLoyaltyPoints").text();
+    let custloyaltyLevel = $(this).find(".loyaltyLevel").text();
+    let custAddress = $(this).find(".customerAddress").text();
+    let custContact = $(this).find(".customerContact").text();
+    let custEmail = $(this).find(".customerEmail").text();
+
+    $("#customer_code").val(custCode);
+    $("#customer_name").val(custName);
+    $("#customer_dob").val(custDOB);
+    $("#customer_gender").val(custGender);
+    $("#customer_join_date").val(custJoinDate);
+    $("#total_points").val(custLoyaltyPoints);
+    $("#loyalty_level").val(custloyaltyLevel);
+    $("#ad_05").val(custAddress);
+    $("#customer_contact").val(custContact);
+    $("#customer_email").val(custEmail);
+})
