473d645aaf4a169aabda985063a360841a460727
Parami <paramiashinsana@gmail.com>
2024-05-07 12:30:41 +0530

    update inventory_service.js

--- js/inventory_service.js
+++ js/inventory_service.js
@@ -35,3 +35,41 @@ function getSupplierName(){
         },
     })
 }
+
+// Save the Inventory
+
+$(document).ready(function() {
+    $('#saveButton').click(function() {
+        var i_code = $('#item_code').val();
+        var I_desc = $('#exampleFormControlInput2').val();
+        var fileInput = $('#profileImg')[0].files[0];
+        var i_category = $('#exampleFormControlInput1').val();
+        var i_qty = $('#exampleFormControlInput2').val();
+        var i_size = $('#exampleFormControlInput1').val();
+        var i_unitPriceSale = $('#exampleFormControlInput2').val();
+        var i_unitPriceBuy = $('#exampleFormControlInput1').val();
+        var i_expectedProfit = $('#exampleFormControlInput2').val();
+        var i_profitMargin = $('#exampleFormControlInput1').val();
+
+        var formData = new FormData();
+
+        formData.append('files', fileInput);
+        formData.append('id', studentId);
+        formData.append('name', studentName);
+
+        $.ajax({
+            url:"http://localhost:9090/api/v1/student/saveStudent",
+            type: 'POST',
+            data: formData,
+            processData: false,
+            mimeType: "multipart/form-data",
+            contentType: false,
+            success: function(response) {
+                console.log('Image uploaded successfully.');
+            },
+            error: function(xhr, status, error) {
+                console.error('Error uploading image:', error);
+            }
+        });
+    });
+});
