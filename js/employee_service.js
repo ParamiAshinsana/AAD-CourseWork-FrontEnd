5a4828b93b87090eb0876ac5e9ba832832c1a12b
Parami <paramiashinsana@gmail.com>
2024-05-10 22:37:55 +0530

    update employee_service.js

--- js/employee_service.js
+++ js/employee_service.js
@@ -26,3 +26,62 @@ function getAllEmployees() {
         }
     });
 }
+
+
+// Save the Employee
+
+$(document).ready(function() {
+    $('#saveButton').click(function() {
+        var emCode = $('#employee_code').val();
+        var emName = $('#employee_name').val();
+        var fileInput = $('#profileImg')[0].files[0];
+        var emDob = $('#emp_dob').val();
+        var emGender = $('#gender').val();
+        var emStatus = $('#employee_status').val();
+        var emDesignation = $('#employee_designation').val();
+        var emAccessRole = $('#access_role').val();
+        var emJoinedDate = $('#join_date').val();
+        var emBranch = $('#emp_branch').val();
+        var emAddress = $('#emp_ad05').val();
+        var emContact = $('#emp_contact').val();
+        var emEmail = $('#emp_email').val();
+        var emGuardianName = $('#emp_guardian').val();
+        var emEmergencyContact = $('#emp_emergency_contact').val();
+
+        var formData = new FormData();
+
+        formData.append('empCode', emCode);
+        formData.append('empName', emName);
+        formData.append('files', fileInput);
+        formData.append('empDOB', emDob);
+        formData.append('empGender', emGender);
+        formData.append('empStatus', emStatus);
+        formData.append('empDesignation', emDesignation);
+        formData.append('empAccessRole', emAccessRole);
+        formData.append('empJoinedDate', emJoinedDate);
+        formData.append('empBranch', emBranch);
+        formData.append('empAddress', emAddress);
+        formData.append('empContact', emContact);
+        formData.append('empEmail', emEmail);
+        formData.append('empGuardianNAme', emGuardianName);
+        formData.append('empEmergencyContact', emEmergencyContact);
+
+
+        $.ajax({
+            url:"http://localhost:8080/api/v1/employee/saveEmployee",
+            type: 'POST',
+            data: formData,
+            processData: false,
+            mimeType: "multipart/form-data",
+            contentType: false,
+            success: function(response) {
+                alert("Saved!!!")
+                console.log('Image uploaded successfully.');
+                getAllEmployees();
+            },
+            error: function(xhr, status, error) {
+                console.error('Error uploading image:', error);
+            }
+        });
+    });
+});
