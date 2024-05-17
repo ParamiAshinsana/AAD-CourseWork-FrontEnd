a569c40c9338180bbcc2b09a9e7e798f08fea2e8
Parami <paramiashinsana@gmail.com>
2024-05-17 17:24:00 +0530

    update sale_services.js

--- js/sale_service.js
+++ js/sale_service.js
@@ -2,3 +2,14 @@ $('#pr_date').click(function () {
     GetTodayDate();
 });
 
+function GetTodayDate() {
+    var tdate = new Date();
+    var dd = tdate.getDate(); //yields day
+    var MM = tdate.getMonth(); //yields month
+    var yyyy = tdate.getFullYear(); //yields year
+    var currentDate = dd + "-" + (MM + 1) + "-" + yyyy;
+
+    // return currentDate;
+
+    $("#pr_date").val(currentDate);
+}
