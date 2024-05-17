29b59c11973d965d360f8a1d1b177dfc6e20cd2c
Parami <paramiashinsana@gmail.com>
2024-05-17 20:15:47 +0530

    update sale_services.js

--- js/sale_service.js
+++ js/sale_service.js
@@ -1,3 +1,21 @@
+getAllCustomers();
+
+// Get All Customer Codes
+function getAllCustomers() {
+    $.ajax({
+        method:"GET",
+        url:"http://localhost:8080/api/v1/suppliers/getAllSupplierCode",
+        async:true,
+        success: function(data) {
+            $("#supp_code").empty();
+            data.forEach(function(supplierCode) {
+                let option = `<option value="${supplierCode}">${supplierCode}</option>`;
+                $("#supp_code").append(option);
+            });
+        }
+    });
+}
+
 $('#pr_date').click(function () {
     GetTodayDate();
 });
