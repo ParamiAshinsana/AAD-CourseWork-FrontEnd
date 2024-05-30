51e15b36b9e05a9c813b01b3baf5b0c9f0fb6f91
Parami <paramiashinsana@gmail.com>
2024-05-31 01:39:52 +0530

    Applying JWT Security to User Registration

--- js/login.js
+++ js/login.js
@@ -9,3 +9,37 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+
+// User Registration
+
+function saveUser() {
+  let userEmail = $('#reg_email').val();
+  let userPassword = $('#reg_password').val();
+  let userName = $('#reg_username').val();
+  let userRole = $('#reg_role').val();
+
+
+  $.ajax({
+    method: "POST",
+    contentType: "application/json",
+    url: "http://localhost:8080/api/v1/user/register",
+    async: true,
+    data: JSON.stringify({
+      "email": userEmail,
+      "password": userPassword,
+      "name": userName,
+      "role": userRole
+
+    }),
+
+    success: function (data) {
+      alert("Saved!!!")
+    },
+
+
+    error: function (xhr, exception) {
+      alert("Error!!!")
+    },
+  })
+}
